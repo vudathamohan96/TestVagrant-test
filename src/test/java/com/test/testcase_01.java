776d43e7d14284c0fb480825048759429cc7f928
vudathamohan96 <62902150+vudathamohan96@users.noreply.github.com>
2022-08-02 18:59:21 +0530

    Update testcase_01.java

--- src/test/java/com/test/testcase_01.java
+++ src/test/java/com/test/testcase_01.java
@@ -15,22 +15,15 @@ import io.restassured.specification.RequestSpecification;
 
 public class testcase_01 {
 
-	/*@BeforeClass
-	public void setup() {
-		RestAssured.baseURI = "https://apigenerator.dronahq.com/api";
-       // RestAssured.basePath = "";
-        RestAssured.requestSpecification = new RequestSpecBuilder().
-                setContentType("application/json").
-                build();
-	}*/
 	  @Test(enabled=true)
 		public void TestCase_01(){
 		  
 			RestAssured.baseURI="https://apigenerator.dronahq.com/api/";
 	    	RequestSpecification req=RestAssured.given();
 	    	Response response=req.request(Method.GET,"/zLGH-lnj/data");
-	    	response.then().body("Wicket-Keeper",Matchers.hasSize(1));
-	    	
+	    	ValidatableResponse check=response.then().body("Wicket-Keeper",Matchers.hasSize(1));
+	    	System.out.println(check.extract().statusCode());
+	    	Assert.assertEquals(check.extract().statusCode(), 200);
 	  }
 	  
 	  }
