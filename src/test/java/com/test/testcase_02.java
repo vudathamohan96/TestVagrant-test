44355a8a7fd9c3e37ebfcaacdd9290089af78fd2
vudathamohan96 <62902150+vudathamohan96@users.noreply.github.com>
2022-08-02 20:06:58 +0530

    Update testcase_02.java

--- src/test/java/com/test/testcase_02.java
+++ src/test/java/com/test/testcase_02.java
@@ -14,20 +14,13 @@ public class testcase_02 {
 	@Test(enabled=true)
 	public void TestCase_02(){
 	  
-		RestAssured.baseURI="https://apigenerator.dronahq.com/api/";
+	RestAssured.baseURI="https://apigenerator.dronahq.com/api";
     	RequestSpecification req=RestAssured.given();
     	Response response=req.request(Method.GET,"/zLGH-lnj/data");
-    	//response.then().body("country",Matchers.hasSize(11));
-    	
-    	JsonPath jsonPathEvaluator = response.jsonPath();
-
-    	
-    	List<String> all = jsonPathEvaluator.getList("country");
-
     
-    	for(String country : all)
-    	{
-    		System.out.println("Country " + country);
-    	}
+    	
+    	List<String> obj=response.path("player[0].findAll{it.country!='India'}");
+    	System.out.println(obj.toString());
+    	System.out.println("Total number of Foreign players: "+obj.size());
     }
 }
